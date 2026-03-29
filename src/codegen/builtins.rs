e5dae6741771193b5855ace11243388c0525ff46
Eoghanmc22 <eoghanmc@icloud.com>
2026-03-28 22:14:02 -0400

    Add syscall

--- src/codegen/builtins.rs
+++ src/codegen/builtins.rs
@@ -1,8 +1,11 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, sync::Arc};
 
 use crate::{
     ast::{IdentRef, Type},
-    codegen::{clac::ClacToken, ir::FunctionSignature},
+    codegen::{
+        clac::{ClacToken, MangledIdent},
+        ir::FunctionSignature,
+    },
 };
 
 pub struct ClacBuiltin {}
@@ -39,6 +42,84 @@ pub fn clac_builtins() -> HashMap<IdentRef<'static>, (ClacToken, FunctionSignatu
             FunctionSignature {
                 arguements: vec![],
                 return_type: Type::Void,
+            },
+        ),
+    );
+
+    map.insert(
+        "syscall",
+        (
+            ClacToken::Call {
+                mangled_ident: MangledIdent(Arc::new("syscall".to_string())),
+                stack_delta: -6,
+            },
+            FunctionSignature {
+                arguements: vec![
+                    (Type::Int, "v6"),
+                    (Type::Int, "v5"),
+                    (Type::Int, "v4"),
+                    (Type::Int, "v3"),
+                    (Type::Int, "v2"),
+                    (Type::Int, "v1"),
+                    (Type::Int, "rax"),
+                ],
+                return_type: Type::Int,
+            },
+        ),
+    );
+
+    map.insert(
+        "write8",
+        (
+            ClacToken::Call {
+                mangled_ident: MangledIdent(Arc::new("syscall".to_string())),
+                stack_delta: -2,
+            },
+            FunctionSignature {
+                arguements: vec![(Type::Int, "val"), (Type::Int, "addr")],
+                return_type: Type::Void,
+            },
+        ),
+    );
+
+    map.insert(
+        "writeNative",
+        (
+            ClacToken::Call {
+                mangled_ident: MangledIdent(Arc::new("syscall".to_string())),
+                stack_delta: -2,
+            },
+            FunctionSignature {
+                arguements: vec![(Type::Int, "val"), (Type::Int, "addr")],
+                return_type: Type::Void,
+            },
+        ),
+    );
+
+    map.insert(
+        "read8",
+        (
+            ClacToken::Call {
+                mangled_ident: MangledIdent(Arc::new("syscall".to_string())),
+                stack_delta: 0,
+            },
+            FunctionSignature {
+                arguements: vec![(Type::Int, "addr")],
+                return_type: Type::Int,
+            },
+        ),
+    );
+
+    map.insert(
+        "readNative",
+        (
+            ClacToken::Call {
+                mangled_ident: MangledIdent(Arc::new("syscall".to_string())),
+                stack_delta: 0,
+            },
+            FunctionSignature {
+                arguements: vec![(Type::Int, "addr")],
+                return_type: Type::Int,
             },
         ),
     );
