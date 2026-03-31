c0513d0fde4c7fa867f536005d1edee659847731
stanleymw <107821509+stanleymw@users.noreply.github.com>
2026-03-30 21:47:25 -0400

    init

---
+++ clac.c
@@ -0,0 +1,143 @@
+typedef struct {
+  int len;
+  char* dat;
+} str;
+
+const int PROT_EXEC = 4;
+const int PROT_READ = 1;
+const int PROT_WRITE = 2;
+const int PROT_NONE = 0;
+
+const int MAP_PRIVATE = 2;
+const int MAP_ANON = 32;
+
+const int STDIN = 0;
+const int STDOUT = 1;
+const int STDERR = 2;
+
+const int NULL = 0;
+
+
+int handle_error(int res) {
+  if (res == -1) {
+    print(67676767);
+    quit();
+  }
+
+  res
+}
+
+char *mmap(char *addr, int length, int prot, int flags, int fd, int pgoffset) {
+  (char *)handle_error(syscall(0x09, (int)addr, length, prot, flags, fd, pgoffset))
+}
+
+char *malloc(int size) {
+  const int prot = PROT_READ + PROT_WRITE;
+  const int flags = MAP_PRIVATE + MAP_ANON;
+
+  mmap((char *)NULL, size, prot, flags, -1, 0)
+}
+
+int read(int fd, char *buf, int count) {
+  syscall(0, fd, (int)buf, count, 0, 0, 0)
+}
+
+int write(int fd, char *buf, int count) {
+  syscall(1, fd, (int)buf, count, 0, 0, 0)
+}
+
+int read_all(int fd, char *buf, int count) {
+  int new = read(fd, buf, count);
+
+  if (new != 0) {
+    new + read_all(fd, buf + new, count - new)
+  } else {
+    0
+  }
+}
+
+int write_all(int fd, char *buf, int count) {
+  int new = write(fd, buf, count);
+
+  if (new != 0) {
+    new + write_all(fd, buf + new, count - new)
+  } else {
+    0
+  }
+}
+
+int strlen(char* q) {
+  if ((int)(*q) == 0) {
+    0
+  } else {
+    1 + strlen(q + 1)
+  }
+}
+
+bool find_newline(char* start, int len) {
+  if (len == 0) {
+    false
+  } else if ((int)(*start) == (int)'\n') {
+    true
+  } else {
+    find_newline(start+1, len-1)
+  }
+}
+
+int readline(str inp_buf) {
+  if (inp_buf.len > 0) {
+    int res = read(STDIN, inp_buf.dat, inp_buf.len);
+
+    if (res != 0) {
+      if (!find_newline(inp_buf.dat, res)) {
+        res + readline((str) struct {
+                   dat = inp_buf.dat + res;
+                   len = inp_buf.len - res;
+                 })
+      } else {
+        res
+      }
+    } else {
+      0
+    }
+  } else {
+    0
+  }
+}
+
+void repl(str inp_buf, str prompt) {
+  write_all(STDOUT, prompt.dat, prompt.len);
+
+  int read_amt = readline(inp_buf);
+
+  write_all(STDOUT, inp_buf.dat, read_amt);
+
+  repl(inp_buf, prompt);
+}
+
+void main() {
+  char* strings = malloc(4096);
+
+  char* prompt = strings;
+  *prompt = "clac-clac>> \0";
+
+  char* welcome = prompt + strlen(prompt) + 2;
+  *welcome = "Welcome to clac.clac\n\0";
+
+  write_all(STDOUT, welcome, strlen(welcome));
+
+  str prompt2 = struct {
+    len = strlen(prompt);
+    dat = prompt;
+  };
+
+  int inplen = 0x2000;
+  str inpbuf = struct {
+    len = inplen;
+    dat = malloc(inplen);
+  };
+
+  repl(inpbuf, prompt2);
+}
+
+main()
---
+++ loop.c
@@ -0,0 +1,36 @@
+typedef struct {
+  int capacity;
+  void* dat;
+} uba;
+
+
+
+uba uba_new() {
+  malloc()
+}
+
+void uba_add(uba* uba, void* x) {
+  (*uba).capacity += 1;
+}
+
+int count_down(int n) {
+  if (n != 0) {
+    count_down(n-1)
+  } else {
+    n
+  }
+}
+
+void lol() {
+  print(123);
+  lol();
+}
+
+
+#[no_mangle]
+void main() {
+  count_down(999999999);
+}
+
+main()
+
